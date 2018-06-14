345afbfbe3bbb83ad1dee5fe5bd69054bb7d9e16
Xiaowen Huang <wenn009@gmail.com>
2018-06-13 23:20:37 -0400

    add sound

--- assets/javascripts/sound.js
+++ assets/javascripts/sound.js
@@ -1,6 +1,6 @@
 // var sound = new Audio("christmas.mp3");
 
-var sound = new Audio("sounds/happybirthday.mp3");
+var sound = new Audio("./assets/sounds/happybirthday.mp3");
 var play = function(){
     sound.play();
     sound.loop = true;
